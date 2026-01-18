823abdf626237dd0f0beeae4b9325200f9fe615d
TranAnhVu0411 <vuta183674@gmail.com>
2026-01-18 16:49:40 +0700

    chore: comment create question api view

--- project/users/views/question_views.py
+++ project/users/views/question_views.py
@@ -23,61 +23,61 @@ class QuestionViewSet(ViewSet):
     (Create, Retrieve, Update security questions)
     """
 
-    def create(self, request):
-        """
-        Endpoint to create security questions
-        """
-        try:
-            logger.info(
-                {
-                    "event_type": EventType.CREATE_SECURITY_QUESTIONS,
-                    "message": "Begin create security questions",
-                }
-            )
-            serializer = CreateSecurityQuestionSerializer(data=request.data, many=True)
-            serializer.is_valid(raise_exception=True)
-            data = serializer.validated_data
-            security_question_instances = [
-                SecurityQuestion(content=question["content"]) for question in data
-            ]
-            SecurityQuestion.objects.bulk_create(security_question_instances)
-            logger.info(
-                {
-                    "event_type": EventType.CREATE_SECURITY_QUESTIONS,
-                    "message": "Create security questions success",
-                }
-            )
-            return JsonResponse(
-                {"message": "Successfully create security questions"},
-                status=status.HTTP_201_CREATED,
-            )
-        except ValidationError as e:
-            logger.error(
-                {
-                    "event_type": EventType.CREATE_SECURITY_QUESTIONS,
-                    "error_type": ErrorTypes.REQUEST_VALIDATION,
-                    "error_content": e.detail,
-                }
-            )
-            return JsonResponse(
-                {
-                    "message": "Invalid request",
-                    "error_content": e.detail,
-                },
-                status=status.HTTP_400_BAD_REQUEST,
-            )
-        except Exception as e:
-            logger.error(
-                {
-                    "event_type": EventType.CREATE_SECURITY_QUESTIONS,
-                    "error_type": ErrorTypes.EXCEPTION,
-                    "error_content": str(e),
-                }
-            )
-            return JsonResponse(
-                {"message": "Internal server error", "error_content": str(e)},
-                status=status.HTTP_500_INTERNAL_SERVER_ERROR,
-            )
+    # def create(self, request):
+    #     """
+    #     Endpoint to create security questions
+    #     """
+    #     try:
+    #         logger.info(
+    #             {
+    #                 "event_type": EventType.CREATE_SECURITY_QUESTIONS,
+    #                 "message": "Begin create security questions",
+    #             }
+    #         )
+    #         serializer = CreateSecurityQuestionSerializer(data=request.data, many=True)
+    #         serializer.is_valid(raise_exception=True)
+    #         data = serializer.validated_data
+    #         security_question_instances = [
+    #             SecurityQuestion(content=question["content"]) for question in data
+    #         ]
+    #         SecurityQuestion.objects.bulk_create(security_question_instances)
+    #         logger.info(
+    #             {
+    #                 "event_type": EventType.CREATE_SECURITY_QUESTIONS,
+    #                 "message": "Create security questions success",
+    #             }
+    #         )
+    #         return JsonResponse(
+    #             {"message": "Successfully create security questions"},
+    #             status=status.HTTP_201_CREATED,
+    #         )
+    #     except ValidationError as e:
+    #         logger.error(
+    #             {
+    #                 "event_type": EventType.CREATE_SECURITY_QUESTIONS,
+    #                 "error_type": ErrorTypes.REQUEST_VALIDATION,
+    #                 "error_content": e.detail,
+    #             }
+    #         )
+    #         return JsonResponse(
+    #             {
+    #                 "message": "Invalid request",
+    #                 "error_content": e.detail,
+    #             },
+    #             status=status.HTTP_400_BAD_REQUEST,
+    #         )
+    #     except Exception as e:
+    #         logger.error(
+    #             {
+    #                 "event_type": EventType.CREATE_SECURITY_QUESTIONS,
+    #                 "error_type": ErrorTypes.EXCEPTION,
+    #                 "error_content": str(e),
+    #             }
+    #         )
+    #         return JsonResponse(
+    #             {"message": "Internal server error", "error_content": str(e)},
+    #             status=status.HTTP_500_INTERNAL_SERVER_ERROR,
+    #         )
 
     def list(self, request):
         """
