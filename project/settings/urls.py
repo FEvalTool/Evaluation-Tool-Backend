d89ced8fc7c570b6b6fdd06b6707227bcdd4649e
TranAnhVu0411 <vuta183674@gmail.com>
2026-02-04 11:24:45 +0700

    refactor: implement re-export and import strat for view

--- project/settings/urls.py
+++ project/settings/urls.py
@@ -18,9 +18,7 @@ Including another URLconf
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from common.views import HealthViewSet
-from users.views.account_views import AccountViewSet
-from users.views.auth_views import AuthViewSet
-from users.views.security_question_views import SecurityQuestionViewSet
+from users.views import AccountViewSet, AuthViewSet, SecurityQuestionViewSet
 
 router = DefaultRouter()
 router.register("health", HealthViewSet, basename="health")
---
+++ project/users/views/__init__.py
@@ -0,0 +1,3 @@
+from .account_views import AccountViewSet
+from .auth_views import AuthViewSet
+from .security_question_views import SecurityQuestionViewSet
